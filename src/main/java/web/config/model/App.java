629e39a0c96f166902a365c1107d59dfb7e54389
Jaconda <jaconda007@rambler.ru>
2026-02-18 01:56:45 +0300

    ReadMe and gitignore

---
+++ src/main/java/web/config/model/App.java
@@ -0,0 +1,21 @@
+package web.hibernate.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication(scanBasePackages =  "web")
+public class App extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
+    }
+
+}
